71bffdbb9ade50a4e3afef0285b3682e010b1e66
Dhinesh Kumar <dhineshbalu97@gmail.com>
2019-01-07 08:06:08 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,58 +1,33 @@
 new validateForm({
      'target': document.querySelector('#firstname'),
      'min': 2,
-     // 'max': 10,
-     // 'alphaNumeric': true,
-     // 'alphaNumericWithSpace': true,
-     //  'alphaNumericWithoutSpace': true,
-     //  'alphaNumericDash': true,
-     //  'lowerCase': true,
-     // 'upperCase': true,
-     // 'startsWithUpperCase': true,
-     // 'startsWithLowerCase': true,
-     // 'notStartWithDigit': true,
-     // 'notEndWithSpecialandNum': true,
-     // 'NocontWhiteSpace': true
+     'max': 10,
+     'alphaNumeric': true,
+     'alphaNumericWithSpace': true,
+      'alphaNumericWithoutSpace': true,
+      'alphaNumericDash': true,
+      'lowerCase': true,
+     'upperCase': true,
+     'startsWithUpperCase': true,
+     'startsWithLowerCase': true,
+     'notStartWithDigit': true,
+     'notEndWithSpecialandNum': true,
+     'NocontWhiteSpace': true
 })
-new validateForm({
-     'target': document.querySelector('#secondname'),
-     'min': 5
-     // 'max': 10,
-     // 'alphaNumeric': true,
-     // 'alphaNumericSpace': true,
-     // 'lowerCase': true,
-     // 'upperCase': true,
-     // 'startsWithUpperCase': true,
-     // 'startsWithLowerCase': true,
-     // 'notStartWithDigit': true
-})
+
 new validateForm({
      'target': document.querySelector('#password1'),
      'min': 5,
      'max': 10,
-     // 'onlyDigits': true,
-     // 'onlyCharL': true,
-     // 'onlyCharU': true,
-     //  'onlyChDtUn': true,
-     // 'onlyDtUcLc': true,
-     // 'onlyDtSp': true,
-     // 'Digitrange': '5-9',
-     // 'charRange': 'a-c',
-     // 'strongPass': true
-})
-new validateForm({
-     'target': document.querySelector('#password2'),
-     'min': 5,
-     'max': 10,
-     // 'onlyDigits': true,
-     // 'onlyCharL': true,
-     // 'onlyCharU': true,
-     // 'onlyChDtUn': true,
-     // 'onlyDtUcLc': true,
-     // 'onlyDtSp': true,
-     // 'Digitrange': '5-9',
-     // 'charRange': 'a-c',
-     // 'strongPass': true
+     'onlyDigits': true,
+     'onlyCharL': true,
+     'onlyCharU': true,
+      'onlyChDtUn': true,
+     'onlyDtUcLc': true,
+     'onlyDtSp': true,
+     'Digitrange': '5-9',
+     'charRange': 'a-c',
+     'strongPass': true
 })
 
 new validateForm({
@@ -71,8 +46,8 @@ new validateForm({
 new validateForm({
      'target': document.querySelector('#date'),
      'format':'dd/mm/yyyy',
-     // 'dateBetween':'1/12/1997-10/12/1997',
-     //  'dateAfter': '12/12/1997'
+     'dateBetween':'1/12/1997-10/12/1997',
+      'dateAfter': '12/12/1997'
      'dateBefore': '12/12/1997'
 })
 new validateForm({
